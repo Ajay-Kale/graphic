602aac4a5eff56c07a5385de8a412d504656efca
Ajay-Kale <ajaykale1565@gmail.com>
2019-10-09 16:38:46 +0530

    aadding all files from  master

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "pagers/home"
+  get "pagers/contact"
+  get "pagers/profile"
+  get "pagers/services"
+  root 'pagers#home'
+end
